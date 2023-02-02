436405cf26a5f0665179a90b9cbba0d5a4ff668e
Tigran Karakhanyan <karakhanyant@gmail.com>
2023-02-02 21:34:12 +0400

    Configure styled-components with Vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components/App";
+import { GlobalStyle } from "./GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <StrictMode>
+      <GlobalStyle />
       <App />
    </StrictMode>
 );
