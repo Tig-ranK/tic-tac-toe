ce23888e90fdffdb7743f268b71f998e34ff1161
Tigran Karakhanyan <karakhanyant@gmail.com>
2023-02-02 20:42:57 +0400

    Set up React@18 project with Vite + add starter files for tic-tac-toe from Front End Mentor

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const App = () => {
+   return <Wrapper>App.jsx</Wrapper>;
+};
+
+const Wrapper = styled.h1`
+  color: limegreen; 
+  font-size: 2rem;
+`
